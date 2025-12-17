758b95ec4516d4546461e94338b747833324091a
javi <jsgaston82@gmail.com>
2025-12-17 08:37:09 +0100

    Update data_collector_yfinance.py

--- data_collector_yfinance.py
+++ data_collector_yfinance.py
@@ -1,12 +1,35 @@
-
-import yfinance as yf, pandas as pd, os
+import yfinance as yf
+import pandas as pd
+import os
 from datetime import datetime
+
 os.makedirs("data", exist_ok=True)
-FILE="data/eurusd_5m.csv"
-df = yf.download("EURUSD=X", interval="5m", period="1d", progress=False)
+
+FILE = "data/eurusd_5m.csv"
+
+print("Downloading EURUSD 5m data...")
+
+df = yf.download(
+    "EURUSD=X",
+    interval="5m",
+    period="1d",
+    progress=False
+)
+
+if df.empty:
+    print("No data returned from Yahoo")
+    exit(0)
+
 df = df.reset_index()
+
 if os.path.exists(FILE):
     old = pd.read_csv(FILE)
-    df = pd.concat([old, df]).drop_duplicates("Datetime")
-df.sort_values("Datetime").to_csv(FILE, index=False)
-print("Updated", datetime.utcnow())
+    df = pd.concat([old, df], ignore_index=True)
+    df = df.drop_duplicates(subset=["Datetime"])
+
+df = df.sort_values("Datetime")
+
+df.to_csv(FILE, index=False)
+
+print("Updated eurusd_5m.csv at", datetime.utcnow())
+
